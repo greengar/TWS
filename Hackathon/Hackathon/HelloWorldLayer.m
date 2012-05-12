162583c6f7850acf367c31c427757a083524d802
Eran Davidov <eran.davidov@gmail.com>
2012-05-12 16:02:35 -0700

    Don't capitalize word.

--- Hackathon/Hackathon/HelloWorldLayer.m
+++ Hackathon/Hackathon/HelloWorldLayer.m
@@ -111,16 +111,10 @@ NSString* const DICTIONARY_FILE = @"CommonWords-SixOrLess";
         NSString* fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSASCIIStringEncoding error:&error];
         self.dictionary = [fileContents componentsSeparatedByString:@"\n"];
         
-        self.textEntryLabel = [CCLabelTTF labelWithString:@"XXXX" fontName:@"Arial-BoldMT" fontSize:30];
-        
-        // Finally
-        self.textEntryFieldCC = [CCTextField textFieldWithLabel:self.textEntryLabel andTextField:self.textEntryFieldUI];
-        self.textEntryFieldCC.position = ccp(200, 200);
-        [self addChild:self.textEntryFieldCC];
-        //[self.textEntryFieldUI becomeFirstResponder];
         
         self.textEntryFieldCC = [CCTextField textFieldWithFieldSize:CGSizeMake(screenSize.width, 30) fontName:@"Arial-BoldMT" andFontSize:20];
         self.textEntryFieldCC.position = ccp(0,210);
+        self.textEntryFieldCC.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
         [self addChild:self.textEntryFieldCC];
         [self.textEntryFieldCC setTextColor:ccWHITE];
         [self.textEntryFieldCC setText:@""];
