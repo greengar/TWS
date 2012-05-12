2483f46151fb223d7bc122f398501700a11e8163
Eran Davidov <eran.davidov@gmail.com>
2012-05-12 13:39:37 -0700

    Monsters. oh my.

---
+++ Hackathon/Hackathon/Monster.h
@@ -0,0 +1,16 @@
+//
+//  Monster.h
+//  Hackathon
+//
+//  Created by Eran Davidov on 5/12/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Monster : CCSprite {
+    
+}
+
+@end
---
+++ Hackathon/Hackathon/Monster.m
@@ -0,0 +1,14 @@
+//
+//  Monster.m
+//  Hackathon
+//
+//  Created by Eran Davidov on 5/12/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Monster.h"
+
+
+@implementation Monster
+
+@end
