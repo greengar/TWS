a3d9a1b3d1ce596f2ed9e6881b36296cfbd48ebe
Eran Davidov <eran.davidov@gmail.com>
2012-05-12 13:20:18 -0700

    Initial cocos2d project.

---
+++ Hackathon/Hackathon/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Hackathon
+//
+//  Created by Eran Davidov on 5/12/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
