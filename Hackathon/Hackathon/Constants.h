d4e477b1a300c012c98eb0928d58c52ec1d8e51f
Elliot Lee <elliot.lee@gmail.com>
2012-05-13 12:48:09 -0700

    increase game length from 10 sec to 120 sec

--- Hackathon/Hackathon/Constants.h
+++ Hackathon/Hackathon/Constants.h
@@ -9,7 +9,7 @@
 #ifndef Hackathon_Constants_h
 #define Hackathon_Constants_h
 
-#define GAME_LENGTH_SECONDS 10 // number of seconds for a game play
+#define GAME_LENGTH_SECONDS 120 // number of seconds for a game play
 #define MONSTER_EVERY_X_SECONDS 5 // for now
 #define MONSTER_MOVE_EVERY_X_SECONDS 3.0 // for now
 #define MONSTER_MOVE_DURATION_SECONDS 30.0 // seconds
