515469a5643d329edc95216094179afdaa90f925
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2012-05-13 16:30:42 -0700

    final merge

--- Hackathon/Hackathon/Constants.h
+++ Hackathon/Hackathon/Constants.h
@@ -12,14 +12,14 @@
 #define GAME_LENGTH_SECONDS 60 // number of seconds for a game play
 #define MONSTER_EVERY_X_SECONDS 5 // for now
 #define MONSTER_MOVE_EVERY_X_SECONDS 3.0 // for now
-#define MONSTER_MOVE_DURATION_SECONDS 30.0 // seconds
+#define MONSTER_MOVE_DURATION_SECONDS 20.0 // seconds
 #define INITIAL_POINTS 50.0; // how much a monster kill is worth initially
 #define POINT_DECREASE_VALUE 5.0;
 #define STAR_THROW_TIME 0.5
 #define BLOOD_MOVE_DURATION_SECONDS 1.5
 #define BOSS_APPEARS_AT_TIME_LEFT 3.0
 #define BOSS_INITIAL_POINTS 200
-#define FIREBALL_EVERY_X_SECONDS 6.0 // for now
+#define FIREBALL_EVERY_X_SECONDS 5.0 // for now
 #define FIREBALL_MOVE_DURATION_SECONDS 3.0 // seconds
 #define NINJA_WALK_ON_SCREEN_TIME 0.5 // remote ninjas showing up animation
 #define BOSS_INITIAL_POINTS 200
